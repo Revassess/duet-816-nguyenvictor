7da2169084667b19c8a32a417a62cd4f5f6d9ba8
nguyenvictor10001@gmail.com <nguyenvictor10001@gmail.com>
2020-08-10 14:18:12 -0400

    Testing code

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+
+		int sum = 0;
+		for(int i = 0; i < arr.length; i++)
+		{
+
+            int temp = arr[i];
+            sum += temp;
+
+        }
+		return sum;
 	}
 }
