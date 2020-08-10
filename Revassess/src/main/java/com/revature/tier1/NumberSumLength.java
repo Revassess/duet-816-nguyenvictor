ee6d3a0e7020c50b00a65b1af1848b5d20b7a021
nguyenvictor10001@gmail.com <nguyenvictor10001@gmail.com>
2020-08-10 14:54:57 -0400

    commit

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,14 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		int sum = 0;
+		int powerLength = String.valueOf(num).length();
+
+		int total1 = (int)Math.pow(num,powerLength);
+		if(num /10 >0)
+		{
+
+		}
 
 		return false;
 	}
