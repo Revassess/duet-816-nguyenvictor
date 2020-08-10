5728205457dbc230037c8d2cfddd1da6481309d2
nguyenvictor10001@gmail.com <nguyenvictor10001@gmail.com>
2020-08-10 17:16:47 -0400

    commit

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,15 +16,15 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://database-1.cbomrvqd9ddj.us-east-2.rds.amazonaws.com:5432/postgres";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "victor_nguyen";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "Youarewhat1";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "increment_number";
 
 	// implement this method to connect to the db and return the connection object
 	public Connection connect(){
